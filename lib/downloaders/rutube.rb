3453cb53c1ea4a7f3791fa47c1bfcb8e7124cf81
weshatheleopard <3384534+weshatheleopard@users.noreply.github.com>
2023-09-25 06:20:02 +0000

    True resolution selector for rutube

--- lib/downloaders/rutube.rb
+++ lib/downloaders/rutube.rb
@@ -22,21 +22,22 @@ class RutubeDownloader < VideoDownloader
   AGENT_ALIAS = 'Windows Firefox' #'
 
   def get_track_list(url)
-    md = url.match(/video\/([0-9a-f]+)/i)
-    video_id = md[1]
+    md = url.match(/video\/(?<video_id>[0-9a-f]+)/i)
+    video_id = md[:video_id]
 
     page = @agent.get "https://rutube.ru/api/play/options/#{video_id}", [], url
     json = JSON.parse(page.content)
 
     res_selection_url = json['video_balancer']['m3u8']
-    res_selection_list = @agent.get res_selection_url
+    res_selection_list = @agent.get(res_selection_url).content
 
-    # Pick the best resolution from the list. In this particular downloader, it is always the same, no need to search
-    track_list_url = res_selection_list.content.split("\n").last
-    track_list = @agent.get(track_list_url).content
+    max_res_playlist_url = res_selection_list.split("#EXT-X-").
+      map{ |entry| entry.match /RESOLUTION=(?<resolution>\d+x\d+).+\n(?<url>.+)\n/m }.
+      max_by{ |entry| (entry && entry[:resolution]).to_i }[:url]
 
-    matches = track_list.scan(/^(.+.ts)$/x)
+    track_list = @agent.get(max_res_playlist_url).content
+    matches = track_list.scan(/^(.+\.ts)$/x)
 
-    [ video_id, matches.map { |track| URI(track_list_url).merge(track.first).to_s } ]
+    [ video_id, matches.map { |track| URI(max_res_playlist_url).merge(track.first).to_s } ]
   end
 end
