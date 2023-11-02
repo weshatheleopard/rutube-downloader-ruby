a291d7370b6dd5264ab2559f91d083dc2aea5e00
weshatheleopard <3384534+weshatheleopard@users.noreply.github.com>
2023-11-02 01:29:37 +0000

    Minor code cleanup

--- lib/downloaders/twitch.rb
+++ lib/downloaders/twitch.rb
@@ -19,22 +19,21 @@ class TwitchDownloader < VideoDownloader
   def get_track_list(url)
     md = url.match(/\.twitch\.tv\/videos\/(?<video_id>\d+)/i)
     video_id = md[:video_id]
-    params = {"operationName"=>"PlaybackAccessToken_Template",
-              "query"=>"query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature   authorization { isForbidden forbiddenReasonCode }   __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature   __typename  }}",
-              "variables"=>{"isLive"=>false, "login"=>"", "isVod"=>true, "vodID"=>video_id, "playerType"=>"site"}}
+    params = {operationName: 'PlaybackAccessToken_Template', query: 'query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {    value    signature   authorization { isForbidden forbiddenReasonCode }   __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isVod) {    value    signature   __typename  }}',
+              variables: { isLive: false, login: '', isVod: true, vodID: video_id, playerType: 'site'}}
 
     json_page = @agent.post('https://gql.twitch.tv/gql', params.to_json,
       { 'Content-Type' => 'text/plain;charset=UTF-8', 'Client-ID' => CLIENT_ID })
 
     json = JSON(json_page.content)
-    signature = json['data']["videoPlaybackAccessToken"]['signature']
-    token = json['data']["videoPlaybackAccessToken"]['value']
+    signature = json['data']['videoPlaybackAccessToken']['signature']
+    token = json['data']['videoPlaybackAccessToken']['value']
 
 
     m3u_data = M3UParser.new(@agent.get("https://usher.ttvnw.net/vod/#{video_id}.m3u8",
                                           { sig: signature, token: token, allow_source: true }).content).parse
 
-    max_res_playlist_url = m3u_data[:entries].max_by{ |entry| entry && entry["BANDWIDTH"].to_i }[:url]
+    max_res_playlist_url = m3u_data[:entries].max_by{ |entry| entry && entry['BANDWIDTH'].to_i }[:url]
 
     track_list = M3UParser.new(@agent.get(max_res_playlist_url).content).extract_tracklist(max_res_playlist_url)
 
