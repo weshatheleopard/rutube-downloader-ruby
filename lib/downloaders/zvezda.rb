106d031ab5b05ca181149b5f718877c896ca3b1e
weshatheleopard <3384534+weshatheleopard@users.noreply.github.com>
2023-12-31 03:45:57 +0000

    Update ZvezdaDownloader to latest site version

--- lib/downloaders/zvezda.rb
+++ lib/downloaders/zvezda.rb
@@ -21,16 +21,22 @@ class ZvezdaDownloader < VideoDownloader
 
   def get_track_list(url)
     page = @agent.get(url)
-    md = page.content.match(/flashvars\s=\s(?<json>\{[^}]+})/i)
-    json = JSON.parse(md[:json])
-    base_url = json['file']
+    md = page.content.match(/(<script[^>]+NEXT_DATA[^>]+>)(?<json>.+?)<\/script>/i)
 
-    m3u_data = M3UParser.new(@agent.get("#{base_url}/index.m3u8").content).parse
-    max_res_playlist = m3u_data[:entries].max_by{ |entry| entry && entry["RESOLUTION"].to_i }[:filename]
-    max_res_playlist_url = "#{base_url}/#{max_res_playlist}"
+    json = JSON.parse(md[:json])
+
+    video_id = json.dig('props', 'pageProps', 'news', 'key' )
+    m3u_url = json.dig('props', 'pageProps', 'news' ,'media', 'video')
+
+    m3u_data_page = @agent.get(m3u_url)
+    m3u_data = M3UParser.new(m3u_data_page.content).parse
+
+    max_res_playlist_name = m3u_data[:entries].max_by{ |entry| entry && entry["RESOLUTION"].to_i }[:filename]
+    max_res_playlist_url = m3u_data_page.uri
+    max_res_playlist_url.path = max_res_playlist_url.path.gsub(/([^\/]+?)$/, max_res_playlist_name)
 
     track_list = M3UParser.new(@agent.get(max_res_playlist_url).content).extract_tracklist(max_res_playlist_url)
 
-    [ base_url[-20...-4], track_list ]
+    [ video_id[0...-5], track_list ]
   end
 end
