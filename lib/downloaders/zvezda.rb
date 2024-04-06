eec00e6d346ddf00817012b2097c899f560d2b36
weshatheleopard <3384534+weshatheleopard@users.noreply.github.com>
2024-04-06 22:58:01 +0000

    Older videos on Zvezds use a different video engine

--- lib/downloaders/zvezda.rb
+++ lib/downloaders/zvezda.rb
@@ -22,13 +22,21 @@ class ZvezdaDownloader < VideoDownloader
   def get_track_list(url)
     page = @agent.get(url)
     md = page.content.match(/(<script[^>]+NEXT_DATA[^>]+>)(?<json>.+?)<\/script>/i)
-
-    json = JSON.parse(md[:json])
-
-    video_id = json.dig('props', 'pageProps', 'news', 'key' )
-    m3u_url = json.dig('props', 'pageProps', 'news' ,'media', 'video')
-    title = json.dig('props', 'pageProps', 'news', 'title' )
-    created = json.dig('props', 'pageProps', 'news', 'dateCreate' )
+    if md.nil? then # Older player
+      md = page.content.match(/<script>self.__next_f.push\((?<json>\[1,"12:(.+?)\\n"\])\)<\/script>/)
+      js1 = JSON.parse(md[:json])
+      hsh = JSON::parse(js1.last[3...-1]).dig(0, 0, 3, 'children', 0, 0, 3, 'data', 'items', 0)
+      m3u_url = hsh.dig('media','video','url')
+      title = hsh['title']
+      created = hsh["dateCreate"]
+      video_id = m3u_url.match(/\/(?<video_id>[0-9A-Z]+)\.mp4/i)[:video_id]
+    else # New player
+      json = JSON.parse(md[:json])
+      video_id = json.dig('props', 'pageProps', 'news', 'key' )[0...-5]
+      m3u_url = json.dig('props', 'pageProps', 'news' ,'media', 'video')
+      title = json.dig('props', 'pageProps', 'news', 'title' )
+      created = json.dig('props', 'pageProps', 'news', 'dateCreate' )
+    end
 
     m3u_data_page = @agent.get(m3u_url)
     m3u_data = M3UParser.new(m3u_data_page.content).parse
@@ -43,6 +51,6 @@ class ZvezdaDownloader < VideoDownloader
 
     track_list = M3UParser.new(@agent.get(max_res_playlist_url).content).extract_tracklist(max_res_playlist_url)
 
-    { id: video_id[0...-5], track_list: track_list, title: title, created: created, resolution: max_res_entry["RESOLUTION"] }
+    { id: video_id, track_list: track_list, title: title, created: created, resolution: max_res_entry["RESOLUTION"] }
   end
 end
